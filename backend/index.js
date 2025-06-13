b8a8467a86cbc43b3f4fef73c8f98722eba6c209
Ayush Sharma <ayush.sharmaod21@gmail.com>
2025-06-14 01:06:07 +0530

    Project Setup

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors()); // Allow frontend access
+app.use(express.json());
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from backend!' });
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:8000`);
+});
