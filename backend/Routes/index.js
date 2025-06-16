67de7317bb1aef621c4b13a61ed494959411e956
GoDofThunder9 <tiwariraj1202@gmail.com>
2025-06-16 15:56:48 +0530

    done with the basic setup

---
+++ backend/Routes/index.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const route =express.Router();
+console.log("router loaded");
+// route.use('/',require('./authRoutes'));
+
+module.exports = route;
--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,29 @@
-const express = require('express');
+const express = require("express");
+const dotenv = require("dotenv").config(); // Load environment variables
+const mongoose = require("./config/mongoose"); // Import the Mongoose setup
 const cors = require('cors');
-
+const cookieParser = require("cookie-parser");
+const path = require('path');
 const app = express();
-app.use(cors()); // Allow frontend access
-app.use(express.json());
+app.use('/api/assets', express.static(path.join(__dirname,'assets')));
+const corsOptions = {
+  origin: "http://localhost:5173", // Allow all origins (You can specify your frontend domain here)
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+}
 
-app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from backend!' });
-});
+app.use(cors(corsOptions))
+app.use(express.json()); 
+app.use(cookieParser());
+app.use(express.urlencoded({extended:true}));
 
-console.log("Yash Rawat");
-const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:8000`);
+// app.use("/asset", express.static("asset"));
+app.use("/api/", require("./Routes"));
+const port = process.env.PORT || 8080; // Default to port 3000 if PORT is not set
+app.listen(port, (err) => {
+  if (err) {
+    console.log("Error:", err);
+  } else {
+    console.log("Server is running on Port", port);
+  }
 });
