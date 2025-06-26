cd8e1d42b684b678e254752c6dfe3443ee1a4de8
GoDofThunder9 <tiwariraj1202@gmail.com>
2025-06-26 23:52:19 +0530

    added some controller

--- backend/Routes/Profileroutes.js
+++ backend/Routes/Profileroutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Profilecontroller = require('../controller/Profilecontroller')
+
 router.patch('/nameupdate/:id/profileName', Profilecontroller.nameupdate);
 router.patch('/deliveryaddress/:id/delivery-Address', Profilecontroller.updateDeliveryAddress);
 router.post('/verifyphoneupdate', Profilecontroller.verifyOTP);
@@ -8,5 +9,6 @@ router.patch('/billingaddress/:id/billingAddress',Profilecontroller.updateBillin
 router.delete('/deleteuser/:id',Profilecontroller.deleteUserAccount);
 router.get('/newsletter/:id',Profilecontroller.toggleNewsletterPreference);
 router.get('/messageforuser/:id',Profilecontroller.toggleMessagePreference);
+router.get('/userdata/:id',Profilecontroller.getUserProfile);
 
 module.exports = router
