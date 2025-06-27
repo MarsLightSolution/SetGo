727ae6872a3aa1de81398539b0d813f770b0734e
GoDofThunder9 <tiwariraj1202@gmail.com>
2025-06-28 01:01:29 +0530

    done with integration

--- backend/Routes/Profileroutes.js
+++ backend/Routes/Profileroutes.js
@@ -10,5 +10,6 @@ router.delete('/deleteuser/:id',Profilecontroller.deleteUserAccount);
 router.get('/newsletter/:id',Profilecontroller.toggleNewsletterPreference);
 router.get('/messageforuser/:id',Profilecontroller.toggleMessagePreference);
 router.get('/userdata/:id',Profilecontroller.getUserProfile);
+router.post('/emailverify',Profilecontroller.verifyEmail);
 
 module.exports = router
