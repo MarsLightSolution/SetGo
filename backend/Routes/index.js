515a613ff669f1e5b47b1c4e878e2343a1ae7fbc
ashsish1801 <ashishagrawal0181@gmail.com>
2025-06-29 00:41:36 +0530

    Created jwt middleware

--- backend/Routes/index.js
+++ backend/Routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const productRoutes = require("./product.route.js");
+const verifyJWT = require("../middlewares/jwt.middleware.js");
 console.log("router loaded");
 // console.log( productRoutes);
 router.use('/', require('./Authroutes.js'));
@@ -8,5 +9,5 @@ router.use('/users', require('./userRouter.js'));
 router.use('/redis', require('./redisRoutes.js'));
 router.use('/', require('./Twillioroutes.js'));
 router.use('/', require('./Profileroutes.js'));
-router.use("/api/products",productRoutes);
+router.use("/api/products", verifyJWT ,productRoutes);
 module.exports = router;
