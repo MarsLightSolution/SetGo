a12a8da78eb41eceb68a89d2fa6fc4bb4876217d
Aditya Tiwari <128233988+Adi9425@users.noreply.github.com>
2025-07-04 16:50:18 +0530

    e-wallet router and controller

--- backend/Routes/transaction.route.js
+++ backend/Routes/transaction.route.js
@@ -1,65 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const {fundTransfer,verifyUser} =require("../controller/transaction.controller.js");
 const Transaction = require('../models/transaction.model.js');
 const verifyJWT = require('../middlewares/auth.middlewares.js');
 const { Message } = require('twilio/lib/twiml/MessagingResponse.js');
 const User =require("../models/user.js");
+
 // transfer money from one account to another
-router.post("/transferFund", verifyJWT ,async(req,res)=>{
-    try {
-        const newTransaction = new Transaction(req.body);
-        await newTransaction.save();
 
-        await User.findByIdAndUpdate(req.body.sender,{
-            $inc:{balance: -req.body.amount},
-        });
-
-        await User.findByIdAndUpdate(req.body.receiver,{
-            $inc:{balance : req.body.amount}
-        });
-
-        res.send({
-            Message:"Transaction successful",
-            data:newTransaction,
-            success:true,
-        });
-    } catch(error){
-        res.send({
-            Message:"Transaction failed",
-            data:error.Message,
-            success:false,
-        });
-    }
-
-});
 
 // verify the transaction
-router.post("/verifyTransaction", verifyJWT ,async(req,res)=>{
-    try{
-        const user = await User.findOne({_id:req.body.receiver});
-        if(user){
-            res.send({
-                Message:"Account Verified",
-                data:user,
-                success:true,
-            });
-        }
-        else{
-            res.send({
-                Message:"Account not found",
-                data:null,
-                success:false,
-            });
-        }
-    }catch(error){
-        res.send({
-            Message:"Account not found",
-            data:error.Message,
-            success:false,
-        });
-    }
-
-});
-
+router.post("/verifyTransaction", verifyJWT ,verifyUser);
+router.post("/transferFund", verifyJWT ,fundTransfer);
 
 module.exports = router;
