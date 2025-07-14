1c578458311b65b041160870ebe88c20b511490d
GoDofThunder9 <tiwariraj1202@gmail.com>
2025-07-14 16:22:40 +0530

    still changeing the same error once again

--- Frontend/src/components/common/Navbar.jsx
+++ Frontend/src/components/common/Navbar.jsx
@@ -190,8 +190,8 @@ const Navbar = () => {
                       { label: "Show", path: "/userinfo" },
                       { label: "Settings", path: "/profile" },
                       { label: "Watchlist", path: "/watchlist" },
-                      { label: "Users", path: "/users" },
-                      { label: "Search Request", path: "/search-request" },
+                      { label: "Users", path: "/userpage" },
+                      { label: "Search Request", path: "/mysearch" },
                     ].map((item, index) => (
                       <div
                         key={index}
