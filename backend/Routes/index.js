7673e3ba3f8e903d9da145cb93acab1495946522
Shreyanshsahu14 <shreyanshsahu1410@gmail.com>
2025-07-31 00:48:26 +0530

    Accesstoken declared in home.jsx

--- backend/Routes/index.js
+++ backend/Routes/index.js
@@ -11,7 +11,6 @@ router.use('/', require('./Twillioroutes.js'));
 router.use('/', require('./Profileroutes.js'));
 router.use("/api/products" ,productRoutes);
 router.use('/api/chat',require('./chat.js'));
-router.use('/api/user/',require('./WishlistRoutes.js'));
 router.use("/api/transaction",transaction);
 router.use('/api/payment', require('./paymentroutes.js'));
 router.use("/",require("./Followerroutes.js"));
