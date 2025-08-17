b7a94dbde60c4822cb7cd775ff044ae177f6aae8
GoDofThunder9 <tiwariraj1202@gmail.com>
2025-08-17 14:57:40 +0530

    solved duplicate condition

--- Frontend/src/pages/Form.jsx
+++ Frontend/src/pages/Form.jsx
@@ -176,7 +176,6 @@ if (name === "condition") {
       "category",
       "condition",
       "price",
-      "condition",
       "description",
       "postalCode",
       "location",
@@ -233,7 +232,6 @@ if (name === "condition") {
           category: "",
           condition: "",
           price: "",
-          condition: "",
           description: "",
           postalCode: "",
           location: "",
@@ -322,20 +320,6 @@ if (name === "condition") {
                 <MenuItem value="Leisure, Hobby & Neighborhood">{t("home.category.leisureHobbyNeighborhood")}</MenuItem>
                 <MenuItem value="Service">{t("home.category.service")}</MenuItem>
                 <MenuItem value="Other">{t("home.category.other")}</MenuItem>
-              </TextField>
-              <TextField
-                select
-                label="Condition"
-                name="condition"
-                value={formData.condition}
-                onChange={handleChange}
-                sx={{ width: "41rem" }}
-              >
-                <MenuItem value="">Select</MenuItem>
-                <MenuItem value="New">New</MenuItem>
-                <MenuItem value="Like New">Like New</MenuItem>
-                <MenuItem value="Used">Used</MenuItem>
-                <MenuItem value="Defective/Need Repair">Defective/Need Repair</MenuItem>
               </TextField>
               <TextField
                 label={t("form.price")}
