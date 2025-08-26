e8c3811e5080a405e63282fe777335f6a042bfab
GoDofThunder9 <tiwariraj1202@gmail.com>
2025-08-26 13:01:31 +0530

    changes for production

--- backend/index.js
+++ backend/index.js
@@ -18,13 +18,28 @@ dotenv.config();
 const app = express();
 
 // CORS configuration
+const allowedOrigins = [
+  "http://localhost:5173",  // local dev
+  "http://51.20.123.49"     // production Nginx
+];
+
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: function (origin, callback) {
+    // allow requests with no origin (like Postman)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   methods: "GET,POST,PUT,DELETE,PATCH",
   allowedHeaders: "Content-Type,Authorization",
   credentials: true,
 };
+
 app.use(cors(corsOptions));
+
 
 // Setup access log stream
 const accessLogStream = fs.createWriteStream(
