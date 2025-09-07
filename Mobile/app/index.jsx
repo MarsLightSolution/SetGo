2ef8ac36876617f1c0bf52eacef296471e6604f8
GoDofThunder9 <tiwariraj1202@gmail.com>
2025-09-07 10:58:30 +0530

    added basic login

--- Mobile/app/index.jsx
+++ Mobile/app/index.jsx
@@ -1,17 +1,5 @@
-import { Text, View ,} from "react-native";
-import { Link } from "expo-router";
+import { Redirect } from "expo-router";
+
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>raj boy is here</Text>
-      <Link  href="/(auth)/signup"> signup </Link>
-      <Link  href="/(auth)"> login </Link>
-    </View>
-  );
+  return <Redirect href="/(tabs)" />;
 }
