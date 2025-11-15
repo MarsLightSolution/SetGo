0a24f9c251122d3289aa98a4f6e8a7ef622dd4ab
Aditya Tiwari <128233988+Adi9425@users.noreply.github.com>
2025-11-15 13:13:16 +0530

    Branch sepration for mobile dev

--- backend/Routes/Twillioroutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const TwillioController = require("../controller/Twilliocontroller");
-router.post("/send-otp", TwillioController.sendOTP);
-router.post("/verify-otp", TwillioController.verifyOTP);
-module.exports = router
-
