abc1dfef8493c91400be0c90f191c363f36b3741
Aditya Tiwari <128233988+Adi9425@users.noreply.github.com>
2025-12-07 12:27:11 +0530

    clear the mobile from main-2

--- Mobile/app/homepage.jsx
+++
@@ -1,6 +0,0 @@
-// app/index.jsx
-import { Redirect } from "expo-router";
-
-export default function homepage() {
-  return <Redirect href="/(tabs)" />; // 👈 This will automatically go to (tabs)/index.jsx
-}
